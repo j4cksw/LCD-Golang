658efd17815a7163c4010b5a686f4eeca1a7232a
Prayoch Rujira <jay@odd-e.co.th>
2015-08-14 18:31:48 +0700

    extract hardcoded third line to getThirdLineForNumber

--- src/lcd.go
+++ src/lcd.go
@@ -4,35 +4,9 @@ func Render(number int) string {
     if number == 1 {
         return getFirstLineForNumber(1) + "\n" + getSecondLineForNumber(1) + "\n" + "|_||_|  |"
     }
-    return getFirstLineForNumber(0) + "\n" + getSecondLineForNumber(0) + "\n" + "|_||_||_|"
+    return getFirstLineForNumber(0) + "\n" + getSecondLineForNumber(0) + "\n" + getThirdLineForNumber(0)
 }
 
-func getFirstLineForNumber(number int) string {
-    return " -  - " + getFirstLineRightDigitForNumber(number)
-}
-
-func getFirstLineRightDigitForNumber(number int) string {
-    return " " + getFirstLineRightDigitCenterForNumber(number) + " "
-}
-
-func getFirstLineRightDigitCenterForNumber(number int) string {
-    if number == 1 {
-        return " "
-    }
-    return "-"
-}
-
-func getSecondLineForNumber(number int) string {
-    return "| || |" + getSecondLineRightDigitForNumber(number)
-}
-
-func getSecondLineRightDigitForNumber(number int) string {
-    return getSecondLineRightDigitLeftForNumber(number) + " |"
-}
-
-func getSecondLineRightDigitLeftForNumber(number int) string {
-    if number == 1 {
-        return " "
-    }
-    return "|"
+func getThirdLineForNumber(number int) string {
+    return "|_||_||_|"
 }
