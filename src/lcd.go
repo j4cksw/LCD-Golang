3b17fa919aa2c5d32b3a48ca0837424e246e4b92
Prayoch Rujira <jay@odd-e.co.th>
2015-08-14 18:40:40 +0700

    extracted duplicate to new function getThirdLineRightDightForNumber

--- src/lcd.go
+++ src/lcd.go
@@ -2,11 +2,18 @@ package lcd
 
 func Render(number int) string {
     if number == 1 {
-        return getFirstLineForNumber(1) + "\n" + getSecondLineForNumber(1) + "\n" + "|_||_|  |"
+        return getFirstLineForNumber(1) + "\n" + getSecondLineForNumber(1) + "\n" + getThirdLineForNumber(1)
     }
     return getFirstLineForNumber(0) + "\n" + getSecondLineForNumber(0) + "\n" + getThirdLineForNumber(0)
 }
 
 func getThirdLineForNumber(number int) string {
-    return "|_||_||_|"
+    return "|_||_|" + getThirdLineRightDightForNumber(number)
+}
+
+func getThirdLineRightDightForNumber(number int) string {
+    if number == 1 {
+        return "  |"
+    }
+    return "|_|"
 }
