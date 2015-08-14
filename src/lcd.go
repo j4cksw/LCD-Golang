b3b27ff6527b195cca92b0bad14fc3d613525ca7
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 01:16:58 +0700

    remove unncessary condition

--- src/lcd.go
+++ src/lcd.go
@@ -1,8 +1,5 @@
 package lcd
 
 func Render(number int) string {
-    // if number == 1 {
-    //     return getFirstLineForNumber(1) + "\n" + getSecondLineForNumber(1) + "\n" + getThirdLineForNumber(1)
-    // }
     return getFirstLineForNumber(number) + "\n" + getSecondLineForNumber(number) + "\n" + getThirdLineForNumber(number)
 }
