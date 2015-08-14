c6067b8133c2f1330019f15198c051fda30c8560
Prayoch Rujira <jay@odd-e.co.th>
2015-08-14 17:06:14 +0700

    add test render 001

--- src/lcd.go
+++ src/lcd.go
@@ -1,5 +1,8 @@
 package lcd
 
 func Render(number int) string {
+    if number == 1 {
+        return " -  -   " + "\n" + "| || |  |" + "\n" + "|_||_|  |"
+    }
     return " -  -  - " + "\n" + "| || || |" + "\n" + "|_||_||_|"
 }
--- src/lcd_test.go
+++ src/lcd_test.go
@@ -2,11 +2,20 @@ package lcd
 
 import "testing"
 
-func Test_render_allZero_getZeroPattern(t *testing.T){
+func Test_render_000_correctly(t *testing.T) {
     result := Render(000)
 
     expectedResult := " -  -  - " + "\n" + "| || || |" + "\n" + "|_||_||_|"
     if result != expectedResult {
         t.Errorf("Expected \n%s but got %s", expectedResult, result)
+    }
+}
+
+func Test_render_001_correctly(t *testing.T) {
+    result := Render(001)
+
+    expectedResult := " -  -   " + "\n" + "| || |  |" + "\n" + "|_||_|  |"
+    if result != expectedResult {
+        t.Errorf("Expected \n%s but got \n%s", expectedResult, result)
     }
 }
