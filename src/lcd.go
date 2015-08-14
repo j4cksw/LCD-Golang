c8de00a400342192bca7c9d4a6707d48142f426f
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 01:15:58 +0700

    replace hardcoded with input arguments

--- src/lcd.go
+++ src/lcd.go
@@ -4,5 +4,5 @@ func Render(number int) string {
     if number == 1 {
         return getFirstLineForNumber(1) + "\n" + getSecondLineForNumber(1) + "\n" + getThirdLineForNumber(1)
     }
-    return getFirstLineForNumber(0) + "\n" + getSecondLineForNumber(0) + "\n" + getThirdLineForNumber(0)
+    return getFirstLineForNumber(number) + "\n" + getSecondLineForNumber(0) + "\n" + getThirdLineForNumber(0)
 }
