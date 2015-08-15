b7f45587323f3d6dac116b0e5288e57377d30934
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 10:51:09 +0700

    replace rendering firstline function with struct

--- src/firstline.go
+++ src/firstline.go
@@ -1,5 +1,22 @@
 package lcd
 
+type FirstLine struct {}
+
+func (l *FirstLine) RenderForValue(value int) string {
+    return " -  - " + l.getFirstLineRightDigitForNumber(value)
+}
+
+func (l *FirstLine) getFirstLineRightDigitForNumber(number int) string {
+    return " " + l.getFirstLineRightDigitCenterForNumber(number) + " "
+}
+
+func (l *FirstLine) getFirstLineRightDigitCenterForNumber(number int) string {
+    if number == 1 {
+        return " "
+    }
+    return "-"
+}
+
 func getFirstLineForNumber(number int) string {
     return " -  - " + getFirstLineRightDigitForNumber(number)
 }
--- src/lcd.go
+++ src/lcd.go
@@ -1,5 +1,6 @@
 package lcd
 
 func Render(number int) string {
-    return getFirstLineForNumber(number) + "\n" + getSecondLineForNumber(number) + "\n" + getThirdLineForNumber(number)
+    firstLine := &FirstLine{}
+    return firstLine.RenderForValue(number) + "\n" + getSecondLineForNumber(number) + "\n" + getThirdLineForNumber(number)
 }
