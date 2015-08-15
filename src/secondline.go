8dcce33cc6b57dbd4d6e1b1a0bfad6a0cebeb8fc
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 10:56:30 +0700

    remove unuse code

--- src/secondline.go
+++ src/secondline.go
@@ -16,18 +16,3 @@ func (l *SecondLine) getSecondLineRightDigitLeftForNumber(number int) string {
     }
     return "|"
 }
-
-func getSecondLineForNumber(number int) string {
-    return "| || |" + getSecondLineRightDigitForNumber(number)
-}
-
-func getSecondLineRightDigitForNumber(number int) string {
-    return getSecondLineRightDigitLeftForNumber(number) + " |"
-}
-
-func getSecondLineRightDigitLeftForNumber(number int) string {
-    if number == 1 {
-        return " "
-    }
-    return "|"
-}
