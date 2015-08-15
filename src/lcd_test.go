289724fe583e016190c8f8c271f55998e7db5b58
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 16:53:25 +0700

    remove test render 002

--- src/lcd_test.go
+++ src/lcd_test.go
@@ -5,6 +5,7 @@ import "testing"
 var expecteds = map[int]string {
     000: " -  -  - " + "\n" + "| || || |" + "\n" + "|_||_||_|" + "\n",
     001: " -  -    " + "\n" + "| || |  |" + "\n" + "|_||_|  |" + "\n",
+    002: " -  -  - " + "\n" + "| || | _|" + "\n" + "|_||_||_ " + "\n",
 }
 
 func Test_render_000(t *testing.T) {
@@ -12,14 +13,5 @@ func Test_render_000(t *testing.T) {
         if Render(value) != expected {
             t.Errorf("Expected \n%s but got %s", expected, Render(value))
         }
-    }
-}
-
-func Test_render_002(t *testing.T) {
-    result := Render(002)
-
-    expectedResult := " -  -  - " + "\n" + "| || | _|" + "\n" + "|_||_||_ " + "\n"
-    if result != expectedResult {
-        t.Errorf("Expected \n%s but got \n%s", expectedResult, result)
     }
 }
