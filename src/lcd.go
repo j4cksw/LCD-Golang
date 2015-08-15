43504a7205afa8d7969101f4851270c674b7ffe7
Prayoch Rujira <jay@odd-e.co.th>
2015-08-15 11:00:58 +0700

    replac third line function to struct

--- src/lcd.go
+++ src/lcd.go
@@ -3,5 +3,6 @@ package lcd
 func Render(number int) string {
     firstLine := &FirstLine{}
     secondLine := &SecondLine{}
-    return firstLine.RenderForValue(number) + "\n" + secondLine.RenderForValue(number) + "\n" + getThirdLineForNumber(number)
+    thirdLine := &ThirdLine{}
+    return firstLine.RenderForValue(number) + "\n" + secondLine.RenderForValue(number) + "\n" + thirdLine.RenderForValue(number)
 }
--- src/thirdline.go
+++ src/thirdline.go
@@ -1,21 +1,23 @@
 package lcd
 
-func getThirdLineForNumber(number int) string {
-    return "|_||_|" + getThirdLineRightDigitForNumber(number)
+type ThirdLine struct {}
+
+func (l *ThirdLine) RenderForValue(number int) string {
+    return "|_||_|" + l.getThirdLineRightDigitForNumber(number)
 }
 
-func getThirdLineRightDigitForNumber(number int) string {
-    return getThirdLineLeftPiece(number) + getThirdLineCenterPiece(number) + "|"
+func (l *ThirdLine) getThirdLineRightDigitForNumber(number int) string {
+    return l.getThirdLineLeftPiece(number) + l.getThirdLineCenterPiece(number) + "|"
 }
 
-func getThirdLineLeftPiece(number int) string {
+func (l *ThirdLine) getThirdLineLeftPiece(number int) string {
     if number == 1 {
         return " "
     }
     return "|"
 }
 
-func getThirdLineCenterPiece(number int) string {
+func (l *ThirdLine) getThirdLineCenterPiece(number int) string {
     if number == 1 {
         return " "
     }
