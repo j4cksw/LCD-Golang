36313b693e31d539d39bbb2dbf5f6209c6c25459
Prayoch Rujira <jay@odd-e.co.th>
2015-08-16 21:09:41 +0700

    remove type ThirdLine

--- src/thirdline.go
+++ src/thirdline.go
@@ -1,28 +1,12 @@
 package lcd
 
-import "fmt"
-
-type ThirdLine struct {
-    segments []Segment
-}
-
 func NewThirdLine() Line {
-    return &ThirdLine{
+    return &ConcreteLine{
+        "|_||_|",
         []Segment{
             NewThirdLineLeftSegment(),
             NewThirdLineCenterSegment(),
             NewThirdLineRightSegment(),
         },
     }
-}
-
-func (l *ThirdLine) RenderForValue(number int) string {
-    return "|_||_|" + l.getThirdLineRightDigitForNumber(number)
-}
-
-func (l *ThirdLine) getThirdLineRightDigitForNumber(number int) string {
-    return fmt.Sprintf("%s%s%s",
-        l.segments[0].RenderForNumber(number),
-        l.segments[1].RenderForNumber(number),
-        l.segments[2].RenderForNumber(number))
 }
