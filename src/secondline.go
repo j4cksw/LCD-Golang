9ad87f2dc1f781605c7a2779c8c4957821a5095a
Prayoch Rujira <jay@odd-e.co.th>
2015-08-16 21:08:25 +0700

    remove type SecondLine

--- src/secondline.go
+++ src/secondline.go
@@ -1,11 +1,5 @@
 package lcd
 
-import "fmt"
-
-type SecondLine struct {
-    segments []Segment
-}
-
 func NewSecondLine() Line {
     return &ConcreteLine{
         "| || |",
@@ -15,15 +9,4 @@ func NewSecondLine() Line {
             NewSecondLineRightSegment(),
         },
     }
-}
-
-func (l *SecondLine) RenderForValue(value int) string {
-    return "| || |" + l.getSecondLineRightDigitForNumber(value)
-}
-
-func (l *SecondLine) getSecondLineRightDigitForNumber(number int) string {
-    return fmt.Sprintf("%s%s%s",
-        l.segments[0].RenderForNumber(number),
-        l.segments[1].RenderForNumber(number),
-        l.segments[2].RenderForNumber(number))
 }
