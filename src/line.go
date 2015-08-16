186f00812b0c31c874beb72e168c078af85c7325
Prayoch Rujira <jay@odd-e.co.th>
2015-08-16 22:48:47 +0700

    rename function getRightDigitForNumber to renderLineDigitForNumber

--- src/line.go
+++ src/line.go
@@ -15,12 +15,12 @@ func (l *Line) RenderForValue(value int) string {
         digit := value%10
         value = value/10
 
-        result = fmt.Sprintf("%s%s", l.getRightDigitForNumber(digit), result)
+        result = fmt.Sprintf("%s%s", l.renderLineDigitForNumber(digit), result)
     }
     return result
 }
 
-func (l *Line) getRightDigitForNumber(number int) string {
+func (l *Line) renderLineDigitForNumber(number int) string {
     return fmt.Sprintf("%s%s%s",
         l.segments[0].RenderForNumber(number),
         l.segments[1].RenderForNumber(number),
