24e4bbbae4b404252a6dba7e1d9bd3f59afede6c
Prayoch Rujira <jay@odd-e.co.th>
2015-08-16 16:04:40 +0700

    remove type ThirdLineCenterSegment user CenterSegment instead.

--- src/thirdline_center_segment.go
+++ src/thirdline_center_segment.go
@@ -1,21 +1,10 @@
 package lcd
 
-type ThirdLineCenterSegment struct {
-    includes map[int]bool
-}
-
-func NewThirdLineCenterSegment() *ThirdLineCenterSegment {
-    return &ThirdLineCenterSegment{
+func NewThirdLineCenterSegment() Segment {
+    return &CenterSegment{
         map[int]bool {
             1:true,
             4:true,
         },
     }
-}
-
-func (s *ThirdLineCenterSegment) RenderForNumber(number int) string {
-    if s.includes[number]{
-        return CENTER_OFF
-    }
-    return CENTER_ON
 }
