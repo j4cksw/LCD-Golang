437987f29f9c6a9b8a0edf3e82cd8bb9c111bcd4
Prayoch Rujira <jay@odd-e.co.th>
2015-08-16 20:52:50 +0700

    remove function getSecondLineRightSegment

--- src/secondline.go
+++ src/secondline.go
@@ -12,9 +12,5 @@ func (l *SecondLine) getSecondLineRightDigitForNumber(number int) string {
     return fmt.Sprintf("%s%s%s",
         NewSecondLineLeftSegment().RenderForNumber(number),
         NewSecondLineCenterSegment().RenderForNumber(number),
-        l.getSecondLineRightSegment(number))
-}
-
-func (l *SecondLine) getSecondLineRightSegment(number int) string {
-    return NewSecondLineRightSegment().RenderForNumber(number)
+        NewSecondLineRightSegment().RenderForNumber(number))
 }
